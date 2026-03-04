09f79b3f4c3b8a78b8bc45348126096a44d9d1ad
Sahil Gupta <guptas3067@gmail.com>
2026-03-04 21:48:05 +0530

    prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,15 @@
-import { Pool } from "pg";
 import { betterAuth } from "better-auth";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "prisma/client";
+
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
   plugins: [tanstackStartCookies()],
-  database: new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl:{rejectUnauthorized: false}
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
   }),
 });
